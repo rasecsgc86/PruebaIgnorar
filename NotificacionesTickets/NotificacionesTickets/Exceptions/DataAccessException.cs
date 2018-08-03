344a3aa4fd125c6c969bac4900fc5cde73bc65f6
ereyesr <ccuervo@visionconsulting.com.mx>
2018-08-03 16:30:18 -0500

    Se agrega Notificaciones

---
+++ NotificacionesTickets/NotificacionesTickets/Exceptions/DataAccessException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NotificacionesTickets.Exceptions
+{
+    public class DataAccessException : Exception
+    {
+        public DataAccessException()
+        {
+            
+        }
+
+        public DataAccessException(string ownMessage)
+            : base(ownMessage)
+        {
+            OwnMessage = ownMessage;
+        }
+
+        public DataAccessException(string ownMessage,
+                                   Exception innerException) : base(ownMessage, innerException)
+        {
+            
+        }
+
+        public string OwnMessage { get; set; }
+    }
+}
---
+++ NotificacionesTickets/NotificacionesTickets/Models/MailModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace NotificacionesTickets.Models
+{
+    public class MailModel
+    {
+        public List<string> MailsTo { set; get; }
+        public List<string> MailsCc { set; get; }
+        public string Subject { set; get; }
+        public string Body { set; get; }
+        public List<string> AttachementsPathsFiles { set; get; }
+    }
+}
