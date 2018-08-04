ddda40ea10cd46875f04161e709be7a4ff9c11bd
ereyesr <ccuervo@visionconsulting.com.mx>
2018-08-03 20:35:46 -0500

    Vaion

--- SolucionMMC.Suite/MMC.MarshLivingSafe.Presentation/app/controllers/configurador/ModalProductosController.js
+++
@@ -1,7 +0,0 @@
-﻿define(['modules/app', 'services/requestService'], function (app) {
-    app.controller('ModalProductosController', ['$scope', '$rootScope', '$location', 'requestService', '$localStorage', 'Notification',
-		function ($scope, $rootScope, $location, requestService, $localStorage, Notification) {
-
-		}
-    ]);
-});
